1e7c91c9e55a9bb9bb0ac07aa4f44980048bd94c
GodReorx <155961655+GodReorx@users.noreply.github.com>
2024-04-02 12:23:09 +0200

    Add files via upload
    Create project

---
+++ src/main/java/cat/itacademy/barcelonactiva/granero/eduard/s04/t01/n02/S04T01N02GraneroEduard/S04T01N02GraneroEduardApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.granero.eduard.s04.t01.n02.S04T01N02GraneroEduard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N02GraneroEduardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N02GraneroEduardApplication.class, args);
+	}
+
+}
